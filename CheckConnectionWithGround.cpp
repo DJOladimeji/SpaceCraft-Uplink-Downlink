048ae306519cabb16b5200515a54d996ac2dab4c
Somto <90286529+chimah1@users.noreply.github.com>
2023-11-09 12:33:23 -0500

    Added Route to check connection to ground

---
+++ CheckConnectionWithGround.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include "crow_all.h"
+#include "connection_checker.h"
+
+using namespace std;
+int source()
+{
+    CROW_ROUTE(app, "/GroundConnection")
+        .methods("GET"_method)
+        ([&](const crow::request& req)
+        {
+                bool connectionStatus = checkConnectionStatus();
+
+                crow::json::wvalue response;
+
+                if (connectionStatus)
+                {
+                    response["status"] = "Connection is established.";
+                }
+                else
+                {
+                    response["status"] = "Connection is lost.";
+                }
+
+                return crow::response(response);
+        });
+}
